7edaed2a2315cae91114b2056a9b428432ee152b
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2022-11-03 21:26:38 +0500

    solved Practice exercise 2.1

---
+++ JavaScript/Chapter 2 JavaScript Essentials/Practice exercise 2.1/app.js
@@ -0,0 +1,12 @@
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+
+console.log(`str1 ${str1}:`, typeof str1) // string
+console.log(`str2 ${str2}:`,typeof str2) // string
+console.log(`val1 ${val1}:`,typeof undefined) // undefined
+console.log(`val2 ${val2}:`,typeof val2) // null
+console.log(`myNum ${myNum}:`,typeof myNum) // number
---
+++ JavaScript/Chapter 2 JavaScript Essentials/Practice exercise 2.1/readme.md
@@ -0,0 +1,8 @@
+# Practice exercise 2.1
+What are the types of these variables listed below? Verify this with typeof and
+output the result to the console:
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
