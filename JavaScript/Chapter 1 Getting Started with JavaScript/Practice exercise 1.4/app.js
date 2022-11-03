ab00d3ff41d5c7dbb88c7f27c5f4da12bff16492
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2022-11-03 20:51:49 +0500

    solved Practice exercise 1.4

---
+++ JavaScript/Chapter 1 Getting Started with JavaScript/Practice exercise 1.4/app.js
@@ -0,0 +1,9 @@
+let a = 10; //10
+
+console.log(a)
+
+
+
+// At the end of your JavaScript code, use a multiple-line comment. In a real
+// production script, you might use this space to add a brief outline of the
+// purpose of the file.
