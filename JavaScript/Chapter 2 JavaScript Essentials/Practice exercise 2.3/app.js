918bb56809380627f743de3d7ad2fc4488e182d0
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2022-11-04 22:35:33 +0500

    Solved Practice exercise 2.3

---
+++ JavaScript/Chapter 2 JavaScript Essentials/Practice exercise 2.3/app.js
@@ -0,0 +1,18 @@
+// question 1
+let a=prompt("Enter the square First value")
+let b=prompt("Enter the square Second value")
+a=Math.sqrt(a)
+b=Math.sqrt(b)
+
+console.log(`square root of value a= ${a} and value b= ${b} `)
+
+
+
+
+// question 1
+let A=10
+let B=10
+let C;
+C=Math.pow(A,2)+Math.pow(B,2)
+C=Math.sqrt(C)
+console.log(`Hypotenuse of a triangle is =${C}`)
