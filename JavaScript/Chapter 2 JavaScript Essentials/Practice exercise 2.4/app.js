2f96dc89518b542731049ff521828e74cc107cdf
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2022-11-04 22:50:29 +0500

    solved Practice exercise 2.4

---
+++ JavaScript/Chapter 2 JavaScript Essentials/Practice exercise 2.4/app.js
@@ -0,0 +1,18 @@
+
+console.log(`Before Update these variables `);
+
+
+let a=10;
+let b=33;
+let c=30;
+console.log(`a= ${a}`);
+console.log(`b= ${b}`);
+console.log(`c= ${c}`);
+
+b=a+b;
+a=a/c;
+c=c%b;
+console.log(` After Update these variables `);
+console.log(`a= ${a}`);
+console.log(`b= ${b}`);
+console.log(`c= ${c}`);
