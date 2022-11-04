03b25f353d9116223e6e872a69f348de1d838cba
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2022-11-05 00:40:44 +0500

    kilo to pounds and CM to inches conversion value solved

---
+++ JavaScript/Chapter 2 JavaScript Essentials/Chapter project/BMI calculator/app.js
@@ -0,0 +1,11 @@
+
+let inch=20;
+let cm
+cm=inch*2.54;
+
+let kilo=20
+let pound
+
+pound=kilo*2.2046;
+console.log(`${inch} inch is equal to ${cm} cm`)
+console.log(`${kilo} kilo is equal to ${pound} pounds`)
