e2ce364059d019db680c05ddb896085ad647d9f5
Asad Iqbal <mianasad18033@gamil.com>
2022-11-06 21:09:40 +0500

    solved exercise 3.1

--- JavaScript/Chapter 3 JavaScript Multiple Values/Practice exercise 3.1/app.js
+++ JavaScript/Chapter 3 JavaScript Multiple Values/Practice exercise 3.1/app.js
@@ -1,1 +1,5 @@
 var shopping=["Milk","Bread", "Apples"]
+console.log(`Before Update shopping list`,shopping)
+ console.log(`Length of shopping:`,shopping.length)
+ shopping.splice(1,1,"Bananas")
+console.log(`After Update shopping list`,shopping)
