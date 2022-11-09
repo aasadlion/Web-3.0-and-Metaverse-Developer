55fab60d149a140bb2cdd9881912431814589346
Muhammad asad iqbal <mianasad18033@gmail.com>
2022-11-09 13:21:44 +0500

    solved Practice exercise 3.5

---
+++ JavaScript/Chapter 3 JavaScript Multiple Values/Practice exercise 3.5/app.js
@@ -0,0 +1,37 @@
+ 
+ let people={
+
+    friends:[]
+ }
+
+
+ let friend1={
+
+    firstNames:"Asad",
+    lastNames:"Iqbal",
+    ID:123,
+ }
+
+ let friend2={
+
+    firstNames:"Ahmad",
+    lastNames:"Iqbal",
+    ID:456,
+ }
+
+ let friend3={
+
+    firstNames:"fahad",
+    lastNames:"Iqbal",
+    ID:789,
+ }
+
+
+people.friends[0]=friend1
+people.friends[1]=friend2
+people.friends[2]=friend3
+
+
+console.log(`Friends :`,people.friends)
+
+ 
