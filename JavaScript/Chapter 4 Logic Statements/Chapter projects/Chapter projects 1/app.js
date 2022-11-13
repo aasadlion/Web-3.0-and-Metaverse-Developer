b0107fdc7feb00e34b255c209d11cdc7bd719a57
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2022-11-13 12:11:53 +0500

    solved Chapter 4 projects 1

--- JavaScript/Chapter 4 Logic Statements/Chapter projects/Chapter projects 1/app.js
+++ JavaScript/Chapter 4 Logic Statements/Chapter projects/Chapter projects 1/app.js
@@ -1,1 +1,24 @@
 let dynamicNumber=100;
+let userNmuber=90;
+let output;
+
+
+if (userNmuber>=dynamicNumber) {
+
+    output= userNmuber +' was greater than'+dynamicNumber
+    
+}
+
+else if(userNmuber==dynamicNumber){
+
+    output= userNmuber +' was equal'+dynamicNumber
+}
+else if(userNmuber<=dynamicNumber){
+
+    output= userNmuber +' was less than'+dynamicNumber 
+}
+else{
+
+    output='change the number';
+
+}
