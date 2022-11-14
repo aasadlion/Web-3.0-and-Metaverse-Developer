f51386d7106111eb9af5246f068e55a9f4e0fffa
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2022-11-14 21:26:58 +0500

    10 % complate Chapter 4 projects 3

--- JavaScript/Chapter 4 Logic Statements/Chapter projects/Chapter projects 3/app.mjs
+++ JavaScript/Chapter 4 Logic Statements/Chapter projects/Chapter projects 3/app.mjs
@@ -1,20 +1,30 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
-console.log(`------------------------------------------------------------------------------------------------`)
+// console.log(`------------------------------------------------------------------------------------------------`)
 
-console.log("Rock Paper Scissors game")
-console.log("0 means: Rock")
-console.log("1 means: Paper")
-console.log("2 means: Scissors")
-console.log(`------------------------------------------------------------------------------------------------`)
+// console.log("Rock Paper Scissors game")
+// console.log("0 means: Rock")
+// console.log("1 means: Paper")
+// console.log("2 means: Scissors")
+// console.log(`------------------------------------------------------------------------------------------------`)
 
 let userPlayer=prompt("User to enter a number between 0-2 \n")
+let result;
+let computer;
+computer=Math.random()*3;
+computer=Math.floor(computer);
 
-let computer=Math.random
+// Rock will beat out Scissors,  Paper will beat out Rock, and  Scissors will beat out Paper
+// 
+// ("0 means: Rock")
+// ("1 means: Paper")
+// ("2 means: Scissors")
 
+if (computer===userPlayer) {
+    result="Draw"
+}
+else if(computer===1)
 
-
-
-
-console.log(`Computer chooes a`)
-console.log(`Player chooes a`)
+console.log(`Computer chooes a `, computer)
+console.log(`Player chooes a `, userPlayer)
+console.log(result)
