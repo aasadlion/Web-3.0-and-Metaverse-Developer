de7b2d485ff48bb1209b14bc48c9b60708477e6b
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2022-11-14 20:54:46 +0500

    started Chapter 4 projects 3

---
+++ JavaScript/Chapter 4 Logic Statements/Chapter projects/Chapter projects 3/app.mjs
@@ -0,0 +1,20 @@
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+console.log(`------------------------------------------------------------------------------------------------`)
+
+console.log("Rock Paper Scissors game")
+console.log("0 means: Rock")
+console.log("1 means: Paper")
+console.log("2 means: Scissors")
+console.log(`------------------------------------------------------------------------------------------------`)
+
+let userPlayer=prompt("User to enter a number between 0-2 \n")
+
+let computer=Math.random
+
+
+
+
+
+console.log(`Computer chooes a`)
+console.log(`Player chooes a`)
