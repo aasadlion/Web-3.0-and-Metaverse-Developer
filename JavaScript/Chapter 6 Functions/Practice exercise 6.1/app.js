2ca9b68f3f7c7c84ddea085494d84cdfd85c354e
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2022-11-17 21:52:35 +0500

    solved Practice exercise 6.1

---
+++ JavaScript/Chapter 6 Functions/Practice exercise 6.1/app.js
@@ -0,0 +1,10 @@
+//See if you can write a function for yourself. We want to write a function that adds 
+// two numbers.
+
+function add(num1,num2) {
+    
+console.log(`add the two number is : ${num1+num1}`)
+
+}
+
+add(12,12)
