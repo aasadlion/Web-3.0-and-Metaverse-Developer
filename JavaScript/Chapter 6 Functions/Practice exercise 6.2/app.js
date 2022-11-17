d8413fed6e2aca807adda61acfcd2aa24b110863
Muhammad Asad Iqbal <mianasad18033@gmail.com>
2022-11-17 22:07:23 +0500

    solved Practice exercise 6.2

---
+++ JavaScript/Chapter 6 Functions/Practice exercise 6.2/app.js
@@ -0,0 +1,12 @@
+let = descriptiveWords=["Happy","Sad","bad","careful","wonderful","Super"];
+
+function fun(){
+
+let username="asad"
+let nameAaj=Math.floor(Math.random()*descriptiveWords.length)
+
+console.log(`${descriptiveWords[nameAaj]} ${username}`)
+
+}
+
+fun();
