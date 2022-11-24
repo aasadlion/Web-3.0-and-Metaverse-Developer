2180fbe225fc7d1ae077079380b3c87e46645606
Muhammad asad iqbal <mianasad18033@gmail.com>
2022-11-24 19:53:30 +0500

    Practice exercise 6.5

---
+++ JavaScript/Chapter 6 Functions/Practice exercise 6.5/function.js
@@ -0,0 +1,12 @@
+//Returning with arrow functions
+
+let addTwoNumber = (x, y) => x + y;
+let result = addTwoNumber(12, 12);
+console.log(result);
+
+//Immediately invoked function expression
+(function() {
+
+    console.log('asad')
+
+})();
